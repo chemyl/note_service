1cfed1887107fa766cc24edb71b36e5e079723e3
Anatolii <chemyl.inc@gmail.com>
2024-11-20 11:02:24 +0600

    update db

--- src/db.rs
+++ src/db.rs
@@ -1,16 +1,15 @@
 use sqlx::{sqlite::SqlitePool, Pool};
 
 pub async fn init_db() -> Pool<sqlx::Sqlite> {
-    let pool = SqlitePool::connect("sqlite:notes.db").await.unwrap();
+    let pool = SqlitePool::connect("sqlite:note_service.db").await.unwrap();     //TODO unwrap -> panic!
     sqlx::query(
         "CREATE TABLE IF NOT EXISTS notes (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             title TEXT NOT NULL,
-            content TEXT NOT NULL
-        )",
+            content TEXT NOT NULL)"
     )
         .execute(&pool)
         .await
-        .unwrap();
-    pool
+        .unwrap();          //TODO unwrap -> panic!
+    pool        // SqlitePool
 }
