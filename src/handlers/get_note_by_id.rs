3a600f211bc12f20a15ba7066723dcf91d1098e6
Anatolii <chemyl.inc@gmail.com>
2024-11-22 16:00:36 +0600

    add find by id handler

--- src/handlers/get_note_by_id.rs
+++ src/handlers/get_note_by_id.rs
@@ -2,11 +2,8 @@ use crate::models::note::Note;
 use actix_web::{web, HttpResponse};
 use sqlx::SqlitePool;
 
-pub async fn get_note_by_id(
-    pool: web::Data<SqlitePool>,
-    path: web::Path<i32>,
-) -> HttpResponse {
-    let id = path.into_inner(); // Получение значения i32
+pub async fn get_note_by_id(pool: web::Data<SqlitePool>, path: web::Path<i32>) -> HttpResponse {
+    let id = path.into_inner();
     let note: Option<Note> = sqlx::query_as!(Note, "SELECT id, title, content FROM notes WHERE id = ?", id)
         .fetch_optional(pool.get_ref())
         .await
